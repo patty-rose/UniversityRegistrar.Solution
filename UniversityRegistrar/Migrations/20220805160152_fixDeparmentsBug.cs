fc621d94066e2916754f66df7cb164a111690795
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-08-05 09:03:11 -0700

    fix Departments SQL bug & add Dept Vs/Cs

---
+++ UniversityRegistrar/Migrations/20220805160152_fixDeparmentsBug.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UniversityRegistrar.Migrations
+{
+    public partial class fixDeparmentsBug : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseDepartment_Department_DepartmentId",
+                table: "CourseDepartment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_DepartmentStudent_Department_DepartmentId",
+                table: "DepartmentStudent");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Department",
+                table: "Department");
+
+            migrationBuilder.RenameTable(
+                name: "Department",
+                newName: "Departments");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Departments",
+                table: "Departments",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseDepartment_Departments_DepartmentId",
+                table: "CourseDepartment",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "DepartmentId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DepartmentStudent_Departments_DepartmentId",
+                table: "DepartmentStudent",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "DepartmentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseDepartment_Departments_DepartmentId",
+                table: "CourseDepartment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_DepartmentStudent_Departments_DepartmentId",
+                table: "DepartmentStudent");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Departments",
+                table: "Departments");
+
+            migrationBuilder.RenameTable(
+                name: "Departments",
+                newName: "Department");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Department",
+                table: "Department",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseDepartment_Department_DepartmentId",
+                table: "CourseDepartment",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "DepartmentId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DepartmentStudent_Department_DepartmentId",
+                table: "DepartmentStudent",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "DepartmentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- UniversityRegistrar/Models/UniversityRegistrarContext.cs
+++ UniversityRegistrar/Models/UniversityRegistrarContext.cs
@@ -5,8 +5,16 @@ namespace UniversityRegistrar.Models
   public class UniversityRegistrarContext : DbContext
   {
     public DbSet<Course> Courses { get; set; }
-    public DbSet<Student> Students { get; set; }
+
+    public DbSet<CourseDepartment> CourseDepartment { get; set; }
+
     public DbSet<CourseStudent> CourseStudent { get; set; }
+
+    public DbSet<Department> Departments { get; set; }
+
+    public DbSet<DepartmentStudent> DepartmentStudent { get; set; }
+
+    public DbSet<Student> Students { get; set; } 
 
     public UniversityRegistrarContext(DbContextOptions options) : base(options) { }
 
